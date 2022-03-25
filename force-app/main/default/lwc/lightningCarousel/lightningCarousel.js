18f0cad2932149feb556073e45f2020f1828ea49
pavanbhat1999 <prbhat07@gmail.com>
2022-03-25 21:00:56 +0530

    user greet prompt changed

--- force-app/main/default/lwc/lightningCarousel/lightningCarousel.js
+++ force-app/main/default/lwc/lightningCarousel/lightningCarousel.js
@@ -1,4 +1,8 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
+//import user data
+import USER from '@salesforce/user/Id';
+import USERNAME from '@salesforce/schema/User.Name';
+import { getRecord } from 'lightning/uiRecordApi';
 //import the static resource
 import image1 from '@salesforce/resourceUrl/img1';
 import image2 from '@salesforce/resourceUrl/img2';
@@ -7,4 +11,24 @@ export default class LightningCarousel extends LightningElement {
 
     img1 = image1;
     img2 = image2;
+
+    //get user details
+    user = USER;
+    @track error;
+    @track name;
+
+    @wire(getRecord, {
+        recordId: USER,
+        fields: [USERNAME]
+    }) wireuser({
+        error,
+        data
+    }) 
+    {
+        if (error) {
+           this.error = error ; 
+        } else if (data) {
+            this.name = data.fields.Name.value;
+        }
+    }
 }
