c88e88b3e5e41486670afc0338d4e130bd23b5bd
pavanbhat1999 <prbhat07@gmail.com>
2022-03-28 10:19:12 +0530

    Partially tested features Auto Population

--- force-app/main/default/lwc/openNewRecord/openNewRecord.js
+++ force-app/main/default/lwc/openNewRecord/openNewRecord.js
@@ -1,15 +1,27 @@
 
 import { LightningElement } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
+import { encodeDefaultFieldValues } from 'lightning/pageReferenceUtils';
 
 export default class OpenNewRecord extends NavigationMixin(LightningElement) {
     navigateToNewOpportunity() {
+        const defaultValues = encodeDefaultFieldValues({
+            Event__c : 'a015j00000BMTOmAAP',
+            Event_Type__c : 'Running',
+            Registered_Event__c:'100M;400M'
+            
+        });
+        console.log(defaultValues);
     this[NavigationMixin.Navigate]({
-        type: 'custom__objectPage',
+        type: 'standard__objectPage',
         attributes: {
             objectApiName: 'Event_Registered__c',
-            actionName: 'new'
-        }
+            actionName: 'new',
+            
+        },
+        state: {
+            defaultFieldValues: defaultValues
+        },
     });
 }
 }
