ef09c658476be5cba15eea51d7c49ab50eb7466a
pavanbhat1999 <prbhat07@gmail.com>
2022-07-07 14:49:52 +0530

    user creation throgh lwc started

--- force-app/main/default/lwc/userCreate/userCreate.js
+++ force-app/main/default/lwc/userCreate/userCreate.js
@@ -9,6 +9,7 @@ import Username from '@salesforce/schema/User.Username';
 import Alias from '@salesforce/schema/User.Alias';
 import LastName from '@salesforce/schema/User.Name';
 import Email from '@salesforce/schema/User.Email';
+import Department from '@salesforce/schema/Contact.Department';
 import TimeZoneSidKey from '@salesforce/schema/User.TimeZoneSidKey';
 import LocaleSidKey from '@salesforce/schema/User.LocaleSidKey';
 import EmailEncodingKey from '@salesforce/schema/User.EmailEncodingKey';
@@ -17,6 +18,7 @@ import LanguageLocaleKey from '@salesforce/schema/User.LanguageLocaleKey';
 
 
 import createUser from '@salesforce/apex/CreateUser.create';
+import assignPerm from '@salesforce/apex/assignPermissionSet.assignPerm';
 
 
 
@@ -24,14 +26,15 @@ export default class UserCreate extends LightningElement {
 
     objectApiName = QUERY__C_OBJECT;
 
-    fields = [Alias,Username, LastName,Email,TimeZoneSidKey,LocaleSidKey,EmailEncodingKey,LanguageLocaleKey];
+    fields = [Alias, LastName, Email, Department];
 
-    Alias = '';
+    // Alias = '';
+    userName = '';
 
-    handleChange(event){
-        this.Alias = event.target.value;
+    // handleChange(event){
+    //     this.Alias = event.target.value;
 
-    }
+    // }
     // connectedCallback(){
     //     createUser().then(
     //         (data) => {
@@ -44,22 +47,48 @@ export default class UserCreate extends LightningElement {
         let field = event.detail.fields;
         console.log('fields  ====> ' + JSON.stringify(field ));
         field.ProfileId = '00e5j000001iwLWAAY';  //putup sys admin id
+        field.TimeZoneSidKey = 'America/New_York';
+        field.LocaleSidKey = 'en_US';
+        field.EmailEncodingKey = 'UTF-8';
+        field.LanguageLocaleKey = 'en_US';
+        //field.FederationIdentifier = 'pvnb@novo.com';
+        field.Username = field.Alias + '@novo.com';
+        this.userName = field.Username;
+
       console.log('fields  After====> ' + JSON.stringify(field ));
         this.template.querySelector('lightning-record-form').submit(field);
        // this.template.querySelector('.recordFormCalss').submit(field);
     }
     handleSuccess(event) {
+        console.log(this.userName);  // save username for retriving userid
 
-        //this.connectedCallback();
-        // perm_id = '0PS5j000004cSjyGAE';
-        createUser({Alias:this.Alias}).then(
+        assignPerm({Name : this.userName}).then(
             (data) => {
-           
-           console.log(this.data);
-           console.log(this.Alias)
-            });
+                console.log(data);
+            }
+        );
+      
 
-        //call imperatively 
+
+        // assign permission set and public groups throgh apex
+
+
+
+
+
+
+
+
+
+        // For direct creation throgh apex
+    //     createUser({Alias:this.Alias}).then(
+    //         (data) => {
+           
+    //        console.log(this.data);
+    //        console.log(this.Alias)
+    //         });
+
+    //     //call imperatively 
     
         const toastEvent = new ShowToastEvent({
             title: 'Query submitted',
@@ -67,7 +96,8 @@ export default class UserCreate extends LightningElement {
             variant: 'success'
         });
         this.dispatchEvent(toastEvent);
-    }
+   
 
 }
 
+}
